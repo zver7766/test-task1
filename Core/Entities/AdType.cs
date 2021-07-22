884f8273b0deea908de5e07b85f25e463aa54a4b
Max Prokhorchuk <star32975@gmail.com>
2021-07-22 21:06:23 +0300

    Add entitites

---
+++ Core/Entities/AdType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public enum AdType
+    {
+        [EnumMember(Value = "TextAd")]
+        TextAd,
+        [EnumMember(Value = "HtmlAd")]
+        HtmlAd,
+        [EnumMember(Value = "BannerAd")]
+        BannerAd,
+        [EnumMember(Value = "VideoAd")]
+        VideoAd
+    }
+}
---
+++ Core/Entities/Advertisement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Entities
+{
+    public class Advertisement
+    {
+        public AdType Type { get; set; } = AdType.TextAd;
+        public string Name { get; set; }
+        public List<Category> Categories { get; set; }
+        public decimal Cost { get; set; }
+        public string Content { get; set; }
+        public int ViewsCount { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
